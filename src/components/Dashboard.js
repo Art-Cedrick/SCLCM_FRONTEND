ddabf4533e6f7f4386ad61f0754af6c7f65acce6
sh1baruuu <devillarudolphangelo@gmail.com>
2025-01-26 23:36:00 +0800

    added date filter in dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,6 +11,10 @@ import {
   Legend,
   ArcElement,
 } from "chart.js";
+import { Controller, useForm } from "react-hook-form";
+import { DatePicker } from "@mui/x-date-pickers";
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import dayjs from "dayjs";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
 
@@ -24,6 +28,20 @@ const Dashboard = () => {
   const [selectedChart, setSelectedChart] = useState(0);
   const [grade, setGrade] = useState("All");
   const [loading, setLoading] = useState(true);
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      startDate: '',
+      endDate: '',
+    },
+  });
+
+
+  const { startDate, endDate } = watch();
 
   const fetchPieChartData = (grade) => {
     const params = grade !== "All" ? { grade } : {};
@@ -110,12 +128,12 @@ const Dashboard = () => {
   useEffect(() => {
     setLoading(true);
     fetchPieChartData(grade);
-    fetchBarChartData(`/familyproblem_analytics/`, setFamilyChartData, "family_problem", "Family Problems", grade);
-    fetchBarChartData(`/friendsproblem_analytics/`, setFriendsChartData, "friends_problem", "Friends Problems", grade);
+    fetchBarChartData(`/familyproblem_analytics/`, setFamilyChartData, "family_problem", "Family Problems", grade, startDate, endDate);
+    fetchBarChartData(`/friendsproblem_analytics/`, setFriendsChartData, "friends_problem", "Friends Problems", grade, startDate, endDate);
     fetchBarChartData(`/healthproblem_analytics/`, setHealthChartData, "health_problem", "Health Problems", grade);
-    fetchBarChartData(`/academicproblem_analytics/`, setAcademicChartData, "academic_problem", "Academic Problems", grade);
-    fetchBarChartData(`/careerproblem_analytics/`, setCareerChartData, "career_problem", "Career Problems", grade);
-  }, [grade]);
+    fetchBarChartData(`/academicproblem_analytics/`, setAcademicChartData, "academic_problem", "Academic Problems", grade, startDate, endDate);
+    fetchBarChartData(`/careerproblem_analytics/`, setCareerChartData, "career_problem", "Career Problems", grade, startDate, endDate);
+  }, [grade, startDate, endDate]);
 
   const handlePieClick = (event, elements) => {
     if (elements.length > 0) {
@@ -144,55 +162,145 @@ const Dashboard = () => {
 
   const mostFrequentCategory = getMostFrequentCategory(chartMap[selectedChart]?.data);
 
+
+
+
   return (
     <div style={styles.container}>
-      <label htmlFor="grade" style={styles.selectLabel}>Select Grade: </label>
-      <select
-        id="grade"
-        value={grade}
-        onChange={(e) => setGrade(e.target.value)}
-        style={styles.select}
-      >
-        {["All", "Kinder", "Grade 1", "Grade 2", "Grade 3", "Grade 4", "Grade 5", "Grade 6", "Grade 7", "Grade 8", "Grade 9", "Grade 10", "Grade 11", "Grade 12", "1st Year", "2nd Year", "3rd Year", "4th Year"].map((level) => (
-          <option key={level} value={level}>
-            {level}
-          </option>
-        ))}
-      </select>
-
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div style={styles.chartContainer}>
-          {/* Card displaying the most frequent category */}
-          <div style={styles.cardContainer}>
-            <div style={styles.card}>
-              <h4>{chartMap[selectedChart].title}</h4>
-              <p>{chartData.datasets[0].data[selectedChart]}</p>
-            </div>
-            <div style={styles.card}>
-              <h4>Most Frequent Problem</h4>
-              <p>{mostFrequentCategory.category}</p>
-            </div>
-          </div>
-
-          <div style={styles.mainContainer}>
-            <div style={styles.chartWrapper}>
-              <div style={styles.pieContainer}>
-                {chartData && <Pie data={chartData} options={{ onClick: handlePieClick }} />}
-              </div>
-
-              {selectedChart !== null && chartMap[selectedChart]?.data && (
-                <div style={styles.barContainer}>
-                  <h2>{chartMap[selectedChart].title}</h2>
-                  <Bar data={chartMap[selectedChart].data} />
-                </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+        <FormControl sx={{ minWidth: 120 }}>
+          <InputLabel id="select-label" sx={{backgroundColor: 'white'}}>Grade/Year</InputLabel>
+          <Select
+            labelId="select-label"
+            id="grade"
+            value={grade}
+            onChange={(e) => setGrade(e.target.value)}
+            sx={{ minWidth: 120 }}
+            size="small"
+          >
+            {["All", "Kinder", "Grade 1", "Grade 2", "Grade 3", "Grade 4", "Grade 5", "Grade 6", "Grade 7", "Grade 8", "Grade 9", "Grade 10", "Grade 11", "Grade 12", "1st Year", "2nd Year", "3rd Year", "4th Year"].map((level) => (
+              <MenuItem value={level}>{level}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <form style={{ display: 'flex', gap: '10px' }} className="space-y-4">
+          <div>
+            <Controller
+              name="startDate"
+              control={control}
+              rules={{ required: 'Start date is required' }}
+              render={({ field: { onChange, value }, fieldState: { error } }) => (
+                <DatePicker
+                  label="Start Date"
+                  value={value ? dayjs(value) : null}
+                  onChange={(date) => {
+                    const formattedDate = date ? dayjs(date).format('MM-DD-YYYY') : null;
+                    onChange(formattedDate);
+                  }}
+                  slotProps={{
+                    textField: {
+                      size: 'small', sx: {
+                        width: '180px',
+                      }
+                    }
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      error={!!error}
+                      helperText={error?.message}
+                      fullWidth
+                    />
+                  )}
+                />
               )}
+            />
+          </div>
+
+          <div>
+            <Controller
+              name="endDate"
+              control={control}
+              rules={{
+                required: 'End date is required',
+                validate: (value) => {
+                  if (!startDate) return true; // Skip validation if `startDate` is not selected
+                  return (
+                    dayjs(value, 'MM-DD-YYYY').isAfter(dayjs(startDate, 'MM-DD-YYYY')) ||
+                    'End date must be after Start Date'
+                  );
+                },
+              }}
+              render={({ field: { onChange, value }, fieldState: { error } }) => (
+                <DatePicker
+                  label="End Date"
+                  value={value ? dayjs(value, 'MM-DD-YYYY') : null} // Convert the value to a Day.js object
+                  onChange={(date) => {
+                    const formattedDate = date ? dayjs(date).format('MM-DD-YYYY') : null; // Format date as MM-DD-YYYY
+                    onChange(formattedDate); // Pass the formatted date to the form
+                  }}
+                  slotProps={{
+                    textField: {
+                      size: 'small', sx: {
+                        width: '180px',
+                      }
+                    }
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      error={!!error} // Highlight the field red if there's an error
+                      helperText={error?.message} // Display error message if validation fails
+                      fullWidth
+                    />
+                  )}
+                />
+              )}
+            />
+            {errors.endDate && (
+              <p className="mt-1 text-sm text-red-500">{errors.endDate.message}</p>
+            )}
+          </div>
+        </form>
+      </div>
+
+
+
+      {
+        loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div style={styles.chartContainer}>
+            {/* Card displaying the most frequent category */}
+            <div style={styles.cardContainer}>
+              <div style={styles.card}>
+                <h4>{chartMap[selectedChart].title}</h4>
+                <p>{chartData.datasets[0].data[selectedChart]}</p>
+              </div>
+              <div style={styles.card}>
+                <h4>Most Frequent Problem</h4>
+                <p>{mostFrequentCategory.category}</p>
+              </div>
+            </div>
+
+            <div style={styles.mainContainer}>
+              <div style={styles.chartWrapper}>
+                <div style={styles.pieContainer}>
+                  {chartData && <Pie data={chartData} options={{ onClick: handlePieClick }} />}
+                </div>
+
+                {selectedChart !== null && chartMap[selectedChart]?.data && (
+                  <div style={styles.barContainer}>
+                    <h2>{chartMap[selectedChart].title}</h2>
+                    <Bar data={chartMap[selectedChart].data} />
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )
+      }
+    </div >
   );
 };
 
